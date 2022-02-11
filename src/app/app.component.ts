74a422e42889f356470f847591549eadfa9f03ee
dylanbore <dylan.bore@cafe-frappe.fr>
2022-02-11 01:44:31 +0100

    Mise en place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Moment} from 'moment';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'travels';
+export class AppComponent implements OnInit {
+
+  travelDate: Moment;
+
+  diff: Moment = moment(moment('2022-03-12').diff(moment()));
+
+  ngOnInit(): void {
+
+    this.travelDate = moment('2022-03-12');
+
+    setInterval((): void => {
+
+      this.diff = moment(this.travelDate.diff(moment()));
+
+    }, 1000);
+  }
 }
